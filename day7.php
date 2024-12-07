f6196401a6398db95c0aca9e8a6865f8148b5c03
Anyvie <anyvie@devlibre.fr>
2024-12-07 22:48:41 +0100

    day7

---
+++ day7.php
@@ -0,0 +1,42 @@
+<?php
+
+$sum1 = $sum2 = 0;
+
+$fd = fopen('input7.txt', 'r');
+while ($row = fgets($fd, 10240)) {
+    list($result, $numbers) = explode(':', $row);
+    $numbers = str_getcsv(trim($numbers), ' ');
+
+    $r = array_filter(f(0, $numbers, $result));
+    if (in_array($result, $r)) $sum1 += $result;
+
+    $r = array_filter(f2(0, $numbers, $result));
+    if (in_array($result, $r)) $sum2 += $result;
+}
+
+echo "Part1: ".$sum1.PHP_EOL;
+echo "Part2: ".$sum2.PHP_EOL;
+
+function f($current, $remaining, $result) {
+    if (! count($remaining)) return [$current];
+
+    $next = array_shift($remaining);
+
+    $r1 = f($current+$next, $remaining, $result);
+    $r2 = f($current*$next, $remaining, $result);
+
+    return array_merge($r1, $r2);
+}
+
+
+function f2($current, $remaining, $result) {
+    if (! count($remaining)) return [$current];
+
+    $next = array_shift($remaining);
+
+    $r1 = f2($current+$next, $remaining, $result);
+    $r2 = f2($current*$next, $remaining, $result);
+    $r3 = f2($current.$next, $remaining, $result);
+
+    return array_merge($r1, $r2, $r3);
+}
